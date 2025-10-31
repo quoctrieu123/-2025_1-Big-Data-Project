edd97902bfb8a0d5913f4ce52d4274f771b12b85
quoctrieu123 <quoctk04@gmail.com>
2025-10-31 10:20:15 +0700

    update producer.py

--- producer/producer_utils_v2.py
+++ producer/producer_utils_v2.py
@@ -17,7 +17,7 @@ load_dotenv()
 # if not OPENWEATHER_API_KEY:
 #     raise ValueError("OPENWEATHER_API_KEY not found in environment variables. Please add it to your .env file.")
 
-OPENWEATHER_API_KEY = "c2c8e579d04e39c65dcf045707527639"
+OPENWEATHER_API_KEY = ""
 
 KAFKA_BROKERS = os.environ.get("KAFKA_EXTERNAL_SERVERS")
 conf = {
