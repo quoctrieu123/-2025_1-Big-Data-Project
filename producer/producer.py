cfb5c65aeee908a0e893bf10c93c3ff2c96839d3
leminhhaii <haile4098@gmail.com>
2025-11-25 16:08:53 +0700

    Update new cities

--- producer/producer.py
+++ producer/producer.py
@@ -27,7 +27,25 @@ if __name__ == "__main__":
         {"name": "Bangkok", "lat": 13.44, "lon": 100.30},
         {"name": "Phnom Penh", "lat": 11.562108, "lon": 104.888535},
         {"name": "Da Nang", "lat": 16.047199, "lon": 108.219955},
-        {"name": "Ho Chi Minh City", "lat": 10.7683, "lon": 106.6758}
+        {"name": "Ho Chi Minh City", "lat": 10.7683, "lon": 106.6758},
+        
+        {"name": "London", "lat": 51.5074, "lon": -0.1278},
+        {"name": "Paris", "lat": 48.8566, "lon": 2.3522},
+        {"name": "Berlin", "lat": 52.5200, "lon": 13.4050},
+        {"name": "Madrid", "lat": 40.4168, "lon": -3.7038},
+        {"name": "Rome", "lat": 41.9028, "lon": 12.4964},
+
+        {"name": "New York", "lat": 40.7128, "lon": -74.0060},
+        {"name": "Los Angeles", "lat": 34.0522, "lon": -118.2437},
+        {"name": "Toronto", "lat": 43.651070, "lon": -79.347015},
+        {"name": "Mexico City", "lat": 19.4326, "lon": -99.1332},
+        {"name": "Buenos Aires", "lat": -34.6037, "lon": -58.3816},
+
+        {"name": "Cairo", "lat": 30.0444, "lon": 31.2357},
+        {"name": "Nairobi", "lat": -1.2921, "lon": 36.8219},
+        {"name": "Lagos", "lat": 6.5244, "lon": 3.3792},
+        {"name": "Johannesburg", "lat": -26.2041, "lon": 28.0473},
+        {"name": "Casablanca", "lat": 33.5731, "lon": -7.5898}
     ]
     
     threads = []
